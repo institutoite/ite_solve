ba7e78b36f9c861c03b8c0cc2456c4bf0aa057b2
pc-ite <informaciones.ite@gmail.com>
2025-02-16 07:12:59 -0400

    produccion 1.0

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\DivisionIteSolveController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/division', [DivisionIteSolveController::class, 'index'])->name('division.index');
+Route::any('/dividir', [DivisionIteSolveController::class, 'dividir'])->name('division.dividir');
+Route::get('/historial', [DivisionIteSolveController::class, 'historial'])->name('division.historial');
+//Route::post('/navegar', [DivisionIteSolveController::class, 'navegar'])->name('division.navegar');
+Route::match(['get', 'post'], '/navegar', [DivisionIteSolveController::class, 'navegar'])->name('division.navegar');
